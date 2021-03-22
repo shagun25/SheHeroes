18d280d80062a425b459138b46eac47547ec2742
Sagar Nath <nathsagar24@gmail.com>
2021-03-22 17:30:49 +0530

    Added Video Capture functionality (In Foreground)

---
+++ lib/pages/video_capture.dart
@@ -0,0 +1,222 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class VideoCapture extends StatefulWidget {
+  static final String route = '/videoCapture';
+
+  VideoCapture();
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoCapture> {
+  CameraController controller;
+  List cameras;
+  int selectedCameraIdx;
+  String videoPath;
+  String albumName = 'Media';
+  String path = null;
+
+  @override
+  void initState() {
+    super.initState();
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+
+      if (cameras.length > 0) {
+        setState(() {
+          selectedCameraIdx = 0;
+        });
+
+        _initCameraController(cameras[selectedCameraIdx]).then((void v) {});
+      } else {
+        print("No camera available");
+      }
+    }).catchError((err) {
+      print('Error: $err.code\nError Message: $err.message');
+    });
+  }
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (controller.value.hasError) {
+        print('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Capture Video'),
+        backgroundColor: Color(0xffb72334),
+      ),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: _cameraPreviewWidget(),
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [_cameraTogglesRowWidget(), _captureControlRowWidget(context), Spacer()],
+              ),
+              SizedBox(height: 20.0)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Display Camera preview.
+  Widget _cameraPreviewWidget() {
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
+    );
+  }
+
+  /// Display the control bar with buttons to take pictures
+  Widget _captureControlRowWidget(context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            FloatingActionButton(
+                child: controller.value.isRecordingVideo?Icon(Icons.stop_circle):Icon(Icons.video_call),
+                backgroundColor: Color(0xffb72334),
+                onPressed: () {
+                  _onCapturePressed(context);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Display a row of toggle to select the camera (or a message if no camera is available).
+  Widget _cameraTogglesRowWidget() {
+    if (cameras == null || cameras.isEmpty) {
+      return Spacer();
+    }
+
+    CameraDescription selectedCamera = cameras[selectedCameraIdx];
+    CameraLensDirection lensDirection = selectedCamera.lensDirection;
+
+    return Expanded(
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: FlatButton.icon(
+            onPressed: _onSwitchCamera,
+            icon: Icon(_getCameraLensIcon(lensDirection)),
+            label: Text("${lensDirection.toString().substring(lensDirection.toString().indexOf('.') + 1)}")),
+      ),
+    );
+  }
+
+  IconData _getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  void _onSwitchCamera() {
+    selectedCameraIdx = selectedCameraIdx < cameras.length - 1 ? selectedCameraIdx + 1 : 0;
+    CameraDescription selectedCamera = cameras[selectedCameraIdx];
+    _initCameraController(selectedCamera);
+  }
+
+  void _onCapturePressed(context) async {
+    // Take the Video in a try / catch block. If anything goes wrong,
+    // catch the error.
+    try {
+      if (!controller.value.isRecordingVideo) {
+        // Attempt to take a video and log where it's been saved
+        path = join(
+          // In this example, store the picture in the temp directory. Find
+          // the temp directory using the `path_provider` plugin.
+          (await getTemporaryDirectory()).path,
+          '${DateTime.now()}.mp4',
+        );
+        print(path);
+        await controller.startVideoRecording(path);
+      }
+      else {
+        await controller.stopVideoRecording();
+        print(path);
+        if (path != null) {
+          GallerySaver.saveVideo(path, albumName: albumName).then((
+              bool success) {
+            setState(() {
+
+            });
+          });
+        }
+        path = null;
+      }
+    }
+    catch (e) {
+      // If an error occurs, log the error to the console.
+      print(e);
+    }
+  }
+
+  void _showCameraException(CameraException e) {
+    String errorText = 'Error: ${e.code}\nError Message: ${e.description}';
+    print(errorText);
+
+    print('Error: ${e.code}\n${e.description}');
+  }
+}
